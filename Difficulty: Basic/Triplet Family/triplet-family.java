bf98e8db6ce98c5195f7eec54bb59065170d7193
AYANDIP DAS <132432126+ayandip108@users.noreply.github.com>
2024-10-27 20:04:47 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Triplet Family/triplet-family.java
@@ -0,0 +1,68 @@
+//{ Driver Code Starts
+// Initial Template for Java
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+
+// } Driver Code Ends
+class Solution {
+    public boolean findTriplet(int[] arr) {
+        int n = arr.length;
+        Arrays.sort(arr);
+        for(int i=n-1; i>=0 ;i--) {
+            int l = 0;
+            int r = i-1;
+            
+            while(l<r) {
+                int sum = arr[l] + arr[r];
+                
+                if(sum==arr[i]) {
+                    return true;
+                } else if(sum<arr[i]) {
+                    l++;
+                } else {
+                    r--;
+                }
+            }
+        }
+        
+        return false;
+    }
+}
+
+
+//{ Driver Code Starts.
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+            String line = br.readLine();
+            String[] tokens = line.split(" ");
+
+            // Create an ArrayList to store the integers
+            ArrayList<Integer> array = new ArrayList<>();
+
+            // Parse the tokens into integers and add to the array
+            for (String token : tokens) {
+                array.add(Integer.parseInt(token));
+            }
+
+            int[] arr = new int[array.size()];
+            int idx = 0;
+            for (int i : array) arr[idx++] = i;
+            Solution obj = new Solution();
+            boolean res = obj.findTriplet(arr);
+            if (res)
+                System.out.println("true");
+            else
+                System.out.println("false");
+
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
