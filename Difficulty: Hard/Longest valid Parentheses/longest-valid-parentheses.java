7a2862190f516773efacd344722d1e3a2bc2f7ab
AYANDIP DAS <132432126+ayandip108@users.noreply.github.com>
2025-02-22 15:18:07 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Longest valid Parentheses/longest-valid-parentheses.java
+++ Difficulty: Hard/Longest valid Parentheses/longest-valid-parentheses.java
@@ -1,47 +1,63 @@
 //{ Driver Code Starts
-//Initial Template for Java
+// Initial Template for Java
 
 import java.io.*;
 import java.util.*;
 
-class GFG{
-    public static void main(String args[])throws IOException
-    {
+class GFG {
+    public static void main(String args[]) throws IOException {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         int t = Integer.parseInt(in.readLine());
-        while(t-- > 0){
+        while (t-- > 0) {
             String S = in.readLine();
-            
+
             Solution ob = new Solution();
             System.out.println(ob.maxLength(S));
+
+            System.out.println("~");
         }
     }
 }
 // } Driver Code Ends
 
 
-//User function Template for Java
 
-class Solution{
-    static int maxLength(String s){
-        // code here
-        int maxLength = 0;
-        Stack<Integer> stack = new Stack<>();
-        stack.push(-1);  
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == '(') {
-                stack.push(i);
-            } else {
-                stack.pop();  
-                
-                if (!stack.isEmpty()) {
-                    maxLength = Math.max(maxLength, i - stack.peek());
-                } else {
-                    stack.push(i); 
-                }
-            }
-        }
-        
-        return maxLength;
+class Solution {
+    static int maxLength(String s) {
+       int n=s.length();
+       int l=0;
+       int r=0;
+       int max=0;
+       for(int  i=0;i<n;i++)
+       {
+           if(s.charAt(i)=='(')
+           l++;
+           if(s.charAt(i)==')')
+           r++;
+           if(l==r)
+           max=Math.max(max,2*l);
+           if(r>l)
+           {
+               l=0;
+               r=0;
+           }
+       }
+       r=0;
+       l=0;
+       for(int i=n-1;i>=0;i--)
+       {
+           if(s.charAt(i)=='(')
+           l++;
+           if(s.charAt(i)==')')
+           r++;
+           if(l==r)
+           max=Math.max(max,2*l);
+           if(r<l)
+           {
+               l=0;
+               r=0;
+           }
+       }
+       return max;
     }
 }
