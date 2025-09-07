5ca9ec878272eb419d1cf7e207c8ad3a2876b3ef
AYANDIP DAS <132432126+ayandip108@users.noreply.github.com>
2025-09-07 21:28:22 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Merge K sorted linked lists/merge-k-sorted-linked-lists.java
+++ Difficulty: Medium/Merge K sorted linked lists/merge-k-sorted-linked-lists.java
@@ -1,109 +1,21 @@
-//{ Driver Code Starts
-import java.util.*;
-
-class Node {
-    int data;
-    Node next;
-
-    Node(int x) {
-        data = x;
-        next = null;
-    }
-}
-
-public class Main {
-    /* Function to print nodes in a given linked list */
-    static void printList(Node node) {
-        while (node != null) {
-            System.out.print(node.data + " ");
-            node = node.next;
-        }
-        System.out.println();
-    }
-
-    // Driver program to test the above functions
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int t = Integer.parseInt(scanner.nextLine());
-
-        while (t-- > 0) {
-            int n = Integer.parseInt(scanner.nextLine());
-            List<Node> lists = new ArrayList<>();
-            for (int i = 0; i < n; i++) {
-                String line = scanner.nextLine();
-                String[] values = line.split(" ");
-                Node head = null, temp = null;
-
-                for (String value : values) {
-                    Node newNode = new Node(Integer.parseInt(value));
-                    if (head == null) {
-                        head = newNode;
-                        temp = head;
-                    } else {
-                        temp.next = newNode;
-                        temp = temp.next;
-                    }
-                }
-
-                lists.add(head);
-            }
-
-            Solution sol = new Solution();
-            Node head = sol.mergeKLists(lists);
-
-            printList(head);
-            System.out.println("~");
-        }
-        scanner.close();
-    }
-}
-
-// } Driver Code Ends
-
-
-// User function Template for Java
-/*class Node
-{
-    int data;
-    Node next;
-
-    Node(int key)
-    {
-        data = key;
-        next = null;
-    }
-}
-*/
-
-// arr is an array of Nodes of the heads of linked lists
-
 class Solution {
-    // Function to merge K sorted linked list.
-    Node mergeKLists(List<Node> arr) {
-        // Add your code here.
-        PriorityQueue<Node> pq=new PriorityQueue<>((a,b)->a.data-b.data);
-        for(Node n:arr)
-        {
-        if (n != null)
-        pq.add(n);
+    Node mergeKLists(Node[] arr) {
+        // code here
+        ArrayList<Integer> al = new ArrayList<>();
+        for(int i=0; i< arr.length; i++){
+            Node curr = arr[i];
+            while(curr !=null){
+                al.add(curr.data);
+                curr = curr.next;
+            }
         }
-        Node head=null,tail=null;
-        while(!pq.isEmpty())
-        {
-            Node temp=pq.poll();
-            if(head==null)
-            {
-                head=temp;
-                tail=temp;
-            }
-            else{
-                tail.next=temp;
-                tail=temp;
-            }
-            if(temp.next!=null)
-            {
-                pq.add(temp.next);
-            }
+        Collections.sort(al);
+        Node head = new Node(al.get(0)) ;
+        Node curr = head;
+        for(int i=1; i<al.size(); i++){
+            Node temp =  new Node(al.get(i));
+            curr.next = temp;
+            curr = curr.next;
         }
         return head;
     }
