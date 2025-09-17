9ce9718fe9d61cf785edadcfd6ba613bdf7e788e
AYANDIP DAS <132432126+ayandip108@users.noreply.github.com>
2025-09-17 22:22:40 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Decode the string/decode-the-string.java
+++ Difficulty: Medium/Decode the string/decode-the-string.java
@@ -1,68 +1,80 @@
-//{ Driver Code Starts
-import java.io.*;
-import java.util.*;
-
-class GFG {
-    public static void main(String args[]) throws IOException {
-        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-        PrintWriter out = new PrintWriter(System.out);
-        int t = Integer.parseInt(in.readLine());
-        while (t-- > 0) {
-            String s = in.readLine();
-
-            Solution ob = new Solution();
-            out.println(ob.decodeString(s));
-
-            out.println("~");
-        }
-        out.close();
-    }
-}
-// } Driver Code Ends
-
-
-
 class Solution {
     static String decodeString(String s) {
         // code here
-        Stack<Integer>val=new Stack<>();
-        Stack<Character>stack=new Stack<>();
-        StringBuilder res=new StringBuilder();
-        for(int i=0;i<s.length();){
-            char ch=s.charAt(i);
-            if(ch>='0'&&ch<='9'){
-                StringBuilder str=new StringBuilder();
-                while(ch>='0'&&ch<='9'){
-                    str.append(ch);
-                    i++;
-                    ch=s.charAt(i);
-                }
-                int cnt=Integer.parseInt(str.toString());
-                val.push(cnt);
-            }else if(ch==']'){
-                StringBuilder str=new StringBuilder();
-                while(!stack.isEmpty()&&stack.peek()!='['){
-                    char ch1=stack.pop();
-                    str.append(ch1);
-                }
-                stack.pop();
-                int times=val.pop();
-                str.reverse();
-                for(int j=0;j<times;j++){
-                    for(int k=0;k<str.length();k++){
-                        stack.push(str.charAt(k));
-                    }
-                }
-                i++;
-            }else{
-                stack.push(ch);
-                i++;
+        Stack<StringBuilder> st = new Stack<>();
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i<s.length(); i++)
+        {
+            char c = s.charAt(i);
+            
+            switch(c)
+            {
+                case '[':
+                    st.push(sb);
+                    sb = new StringBuilder();
+                    break;
+                case ']':
+                    decode(st, sb);
+                    sb = st.pop();
+                    break;
+                
+                default:
+                    sb.append(c);
+            }
+            //System.out.println(st);
+        }
+        return sb.toString();
+    }
+    
+    private static void decode(Stack<StringBuilder> st, StringBuilder sb)
+    {
+        StringBuilder num = st.size() > 0 ? st.pop() : null;
+        
+        if(num == null)
+            return;
+        
+        String val = "";
+        
+        for(int i = num.length()-1; i>= 0; i--)
+        {
+            char c = num.charAt(i);
+            
+            if(isInteger(c))
+            {
+                val = c+val;
+                num.setLength(num.length() - 1);
+            }
+            else
+            {
+                break;
             }
         }
-       while (!stack.isEmpty()) {
-            res.append(stack.pop());
+        
+        if(val.length() == 0)
+        {
+            st.push(num);
+            return;
         }
-        return res.reverse().toString();
+        
+        int freq = Integer.parseInt(val);
+        
+        StringBuilder decoded = getAppended(sb, freq-1);
+        num.append(decoded);
+        st.push(num);
+    }
+    
+    private static StringBuilder getAppended(StringBuilder sb, int freq)
+    {
+        int len = sb.length();
+        for(int i = 0; i<freq; i++)
+        {
+            sb.append(sb.substring(0, len));
+        }
+        return sb;
+    }
+    
+    private static boolean isInteger(char c)
+    {
+        return c >= 48 && c <= 57;
     }
 }
-
