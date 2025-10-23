6088ac940c6c240d52ccf8a1d225a11b88412e01
AYANDIP DAS <132432126+ayandip108@users.noreply.github.com>
2025-10-23 22:28:51 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/K Closest Points to Origin/k-closest-points-to-origin.java
+++ Difficulty: Medium/K Closest Points to Origin/k-closest-points-to-origin.java
@@ -1,57 +1,24 @@
-//{ Driver Code Starts
-import java.util.*;
-
-
-// } Driver Code Ends
 class Solution {
-    public int[][] kClosest(int[][] points, int k) {
-        int res[][] = new int[k][2];
-        Arrays.sort(points, (a, b) -> {
-           return (a[0]*a[0] + a[1]*a[1]) - (b[0]*b[0] + b[1]*b[1]); 
-        });
-        
-        int idx=0;
-        while(idx < k) {
-            res[idx] = points[idx];
-            idx++;
+    public ArrayList<ArrayList<Integer>> kClosest(int[][] points, int k) {
+      
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[0] - b[0]);
+
+        for (int i = 0; i < points.length; i++) {
+            int distance = points[i][0] * points[i][0] + points[i][1] * points[i][1];
+            pq.offer(new int[]{distance, i});
         }
-        
+
+        ArrayList<ArrayList<Integer>> res = new ArrayList<>();
+
+        while (k > 0 && !pq.isEmpty()) {
+            int[] curr = pq.poll();
+            int idx = curr[1];
+            ArrayList<Integer> point = new ArrayList<>();
+            point.add(points[idx][0]);
+            point.add(points[idx][1]);
+            res.add(point);
+            k--;
+        }
         return res;
     }
 }
-
-//{ Driver Code Starts.
-
-public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        int t = scanner.nextInt();
-
-        while (t-- > 0) {
-            int k = scanner.nextInt();
-            int n = scanner.nextInt();
-            int[][] points = new int[n][2];
-            for (int i = 0; i < n; i++) {
-                points[i][0] = scanner.nextInt();
-                points[i][1] = scanner.nextInt();
-            }
-            Solution solution = new Solution();
-            int[][] ans = solution.kClosest(points, k);
-
-            Arrays.sort(ans, (a, b) -> {
-                if (a[0] != b[0]) {
-                    return Integer.compare(a[0], b[0]);
-                }
-                return Integer.compare(a[1], b[1]);
-            });
-            for (int[] point : ans) {
-                System.out.println(point[0] + " " + point[1]);
-            }
-            System.out.println("~");
-        }
-
-        scanner.close();
-    }
-}
-// } Driver Code Ends
