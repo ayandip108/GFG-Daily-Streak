b198b5859705b278d66155b999e9e9017329fefa
AYANDIP DAS <132432126+ayandip108@users.noreply.github.com>
2025-12-26 22:35:24 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Kth Missing Positive Number in a Sorted Array/kth-missing-positive-number-in-a-sorted-array.java
+++ Difficulty: Medium/Kth Missing Positive Number in a Sorted Array/kth-missing-positive-number-in-a-sorted-array.java
@@ -1,59 +1,16 @@
-//{ Driver Code Starts
-// Initial Template for Java
-
-import java.io.*;
-import java.util.*;
-
-class GFG {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int tc = Integer.parseInt(br.readLine().trim());
-
-        while (tc-- > 0) {
-
-            String[] str = br.readLine().trim().split(" ");
-            int[] a = new int[str.length];
-            for (int i = 0; i < str.length; i++) {
-                a[i] = Integer.parseInt(str[i]);
-            }
-            String[] nk = br.readLine().trim().split(" ");
-            int k = Integer.parseInt(nk[0]);
-            Solution sln = new Solution();
-            int ans = sln.kthMissing(a, k);
-
-            System.out.println(ans);
-            System.out.println("~");
-        }
-    }
-}
-// } Driver Code Ends
-
-
 class Solution {
     public int kthMissing(int[] arr, int k) {
         // code here
-        Set<Integer> s = new HashSet<>();
-        for(int i : arr){
-            s.add(i);
-        }
-        
-        int[] arr2 = new int[k];
-        
-        int temp = 1;
-        int x = 0;
-        
-        while(x<k){
-            if(s.contains(temp)){
-                temp++;
-            }
+               ArrayList<Integer> res=new ArrayList<>();
+        int j=0;
+        int l=arr[arr.length-1];
+        for(int i=1;res.size()<k;i++){
+            if(j<arr.length && arr[j]==i){ j++;}
             else{
-                arr2[x] = temp;
-                x++;
-                temp++;
+                res.add(i);
             }
         }
+        return res.get(k-1);
         
-        return arr2[arr2.length-1];
     }
 }
-
