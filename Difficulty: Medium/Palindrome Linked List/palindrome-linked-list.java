47554f3e0be3c38ac3ddc758df5eedef10952a5e
AYANDIP DAS <132432126+ayandip108@users.noreply.github.com>
2025-12-31 20:07:23 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Palindrome Linked List/palindrome-linked-list.java
+++ Difficulty: Medium/Palindrome Linked List/palindrome-linked-list.java
@@ -1,97 +1,51 @@
-//{ Driver Code Starts
-// Initial Template for Java
-
-import java.io.*;
-import java.lang.*;
-import java.util.*;
-
+/*
 class Node {
     int data;
     Node next;
 
-    Node(int data) {
-        this.data = data;
-        this.next = null;
-    }
-}
-
-class Driver_code {
-    static Node insert(Node head, int data) {
-        Node temp = new Node(data);
-        if (head == null) {
-            head = temp;
-            return head;
-        } else {
-            Node t = head;
-            while (t.next != null) {
-                t = t.next;
-            }
-            t.next = temp;
-        }
-        return head;
-    }
-
-    static void printList(Node head) {
-        Node temp = head;
-        while (temp != null) {
-            System.out.print(temp.data + " ");
-            temp = temp.next;
-        }
-    }
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(read.readLine());
-
-        while (t-- > 0) {
-
-            Node head = null;
-
-            String str[] = read.readLine().trim().split(" ");
-            int listSize = str.length;
-            for (int i = 0; i < listSize; i++) {
-                head = insert(head, Integer.parseInt(str[i]));
-            }
-            boolean f = new Solution().isPalindrome(head);
-
-            System.out.println(f ? "true" : "false");
-        }
-    }
-}
-
-// } Driver Code Ends
-
-
-/* Structure of class Node is
-class Node
-{
-    int data;
-    Node next;
-
-    Node(int d)
-    {
+    Node(int d) {
         data = d;
         next = null;
     }
 }*/
 
 class Solution {
-    boolean isPalindrome(Node head) {
-        Node temp=head;
-        String s="";
-        ArrayList<Integer> al=new ArrayList<>();
-         ArrayList<Integer> al1=new ArrayList<>();
-        while(temp!=null)
+    public boolean isPalindrome(Node head) {
+        // code here
+        Node prev = null;
+        Node curr = head;
+        Node head1 = null;
+        Node temp = null;
+        while(curr!=null && curr.next!=null)
         {
-            al.add(temp.data);
-            al1.add(temp.data);
-            temp=temp.next;
+            if(prev==null){
+                prev=head;
+            }else{
+                head1 = prev;
+                prev = prev.next;
+                head1.next = temp;
+                temp = head1;
+            }
+            curr = curr.next.next;
         }
-        Collections.reverse(al);
-        if(al.equals(al1))
-        {
-            return true;
+        if(prev!=null){
+            head1 = prev;
+            if(curr==null)
+                prev = prev.next;
+            else
+                prev = prev.next.next;
+            
+            head1.next = temp;
+            while(prev!=null)
+            {
+                if(head1.data!=prev.data)
+                return false;
+                
+                head1 = head1.next;
+                prev = prev.next;
+            }
         }
-        return false;
+        
+        return true;
     }
 }
