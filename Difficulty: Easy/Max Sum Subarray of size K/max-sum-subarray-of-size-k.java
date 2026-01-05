48c72d41dd654e1ba491578ab0bc706ab9f4eddf
AYANDIP DAS <132432126+ayandip108@users.noreply.github.com>
2026-01-05 20:56:54 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Max Sum Subarray of size K/max-sum-subarray-of-size-k.java
+++ Difficulty: Easy/Max Sum Subarray of size K/max-sum-subarray-of-size-k.java
@@ -1,51 +1,15 @@
-//{ Driver Code Starts
-//Initial Template for Java
-import java.io.*;
-import java.util.*;
-
-class GFG
-{
-    public static void main(String args[])throws IOException
-    {
-        Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
-        while(t-- > 0)
-        {
-            
-            int N=sc.nextInt();
-            int K=sc.nextInt();
-            ArrayList<Integer> Arr = new ArrayList<Integer>(N); 
-  
-            // Appending new elements at 
-            // the end of the list 
-            for (int i = 0; i < N; i++) {
-                int x = sc.nextInt();
-                Arr.add(x); 
-            }
-
-            Solution ob = new Solution();
-            System.out.println(ob.maximumSumSubarray(K,Arr,N));
+class Solution {
+    public int maxSubarraySum(int[] nums, int k) {
+       int windowSum = 0;
+        int maxSum = 0;
+        for (int i = 0; i < k; i++) {
+            windowSum = windowSum + nums[i];
         }
-    }
-}
-
-// } Driver Code Ends
-
-
-//User function Template for Java
-class Solution{
-    static long maximumSumSubarray(int K, ArrayList<Integer> Arr,int N){
-        // code here
-        long curr= 0;
-        for(int i=0; i<K;++i){
-            curr += Arr.get(i);
-            
+        maxSum = windowSum;
+        for (int i = k; i < nums.length; i++) {
+            windowSum = windowSum - nums[i - k] + nums[i];
+            maxSum = Math.max(maxSum, windowSum);
         }
-        long res= curr;
-        for(int i=K; i<N; ++i){
-            curr= curr+ Arr.get(i)- Arr.get(i-K);
-            res= Math.max(res, curr);
-        }
-        return res;
+        return maxSum;
     }
 }
