7a31ae399f1782e7559d333c3f4c20c7951bd984
AYANDIP DAS <132432126+ayandip108@users.noreply.github.com>
2026-01-09 22:53:26 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Subarrays With At Most K Distinct Integers/subarrays-with-at-most-k-distinct-integers.java
+++ Difficulty: Medium/Subarrays With At Most K Distinct Integers/subarrays-with-at-most-k-distinct-integers.java
@@ -1,29 +1,18 @@
-class Solution {
+ class Solution {
     public int countAtMostK(int arr[], int k) {
         // code here
-        if (arr == null || k < 0) {
-            return 0;
-        }
-        int length = arr.length;
-        int count = 0;
-        int left = 0;
-        int right = 0;
-        Map<Integer, Integer> freqMap = new HashMap<>();
-        while (right < length) {
-            int rightChar = arr[right];
-            freqMap.put(rightChar, freqMap.getOrDefault(rightChar, 0) + 1);
-            while (freqMap.size() > k) {
-                int leftChar = arr[left];
-                freqMap.put(leftChar, freqMap.get(leftChar) - 1);
-                if (freqMap.get(leftChar) == 0) {
-                    freqMap.remove(leftChar);
-                }
-                left++;
+        HashMap<Integer, Integer> map=new HashMap<>();
+        int cnt=0,l=0,r=0;
+        while(l<arr.length){
+            map.put(arr[l], map.getOrDefault(arr[l], 0)+1);
+            while(map.size()>k){
+                map.put(arr[r], map.getOrDefault(arr[r], 0)-1);
+                if(map.get(arr[r])==0) map.remove(arr[r]);
+                r++;
             }
-            count += (right - left + 1);
-            right++;
+            cnt += l-r+1;
+            l++;
         }
-        return count;
+        return cnt;
     }
 }
- 
