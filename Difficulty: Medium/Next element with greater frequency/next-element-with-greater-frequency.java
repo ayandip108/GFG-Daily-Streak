8947171f3cee312738695d4527ae33ef95d32af5
AYANDIP DAS <132432126+ayandip108@users.noreply.github.com>
2026-01-18 23:18:17 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Next element with greater frequency/next-element-with-greater-frequency.java
+++ Difficulty: Medium/Next element with greater frequency/next-element-with-greater-frequency.java
@@ -1,27 +1,27 @@
 class Solution {
-    public ArrayList<Integer> findGreater(int[] arr) {
-        int n = arr.length;
-        
-        Map<Integer, Integer> map = new HashMap<>();
-        
-        for(int ele : arr) {
-            map.put(ele, map.getOrDefault(ele, 0) + 1);
+    public ArrayList<Integer> nextFreqGreater(int[] arr) {
+        // code here
+        HashMap<Integer , Integer>map = new HashMap<>();
+        ArrayList<Integer>result = new ArrayList<>();
+        Stack<Integer>st = new Stack<>();
+        for(int i=0;i<arr.length;i++){
+            map.put(arr[i], map.getOrDefault(arr[i],0)+1);
         }
-        ArrayList<Integer> ll = new ArrayList<>(Collections.nCopies(n, -1));
-        
-        Stack<Integer> stk = new Stack<>();
-        for(int i =n-1; i >= 0; i--) {
-            while(!stk.isEmpty() && map.get(stk.peek()) <= map.get(arr[i])) {
-                stk.pop();
+        int i=arr.length-1;
+        while(i>=0){
+            while(!st.isEmpty()&& (map.get(st.peek())<=map.get(arr[i]))){
+                st.pop();
             }
-            if(stk.isEmpty()) {
-                ll.set(i, -1);
-            } else {
-                ll.set(i, stk.peek());
+            if(st.isEmpty()){
+                result.add(-1);
             }
-            stk.push(arr[i]);
+            else{
+                result.add(st.peek());
+            }
+            st.push(arr[i]);
+            i--;
         }
-        return ll;
-        
+        Collections.reverse(result);
+        return result;
     }
 }
