99bcd7614983e4b972adf713e3ec4bdde1a81f6c
AYANDIP DAS <132432126+ayandip108@users.noreply.github.com>
2026-02-11 23:10:25 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Equalize the Towers/equalize-the-towers.java
+++ Difficulty: Medium/Equalize the Towers/equalize-the-towers.java
@@ -1,45 +1,42 @@
 class Solution {
     public int minCost(int[] heights, int[] cost) {
         // code here
-          int low = Integer.MAX_VALUE;
-        int high = Integer.MIN_VALUE;
-         for (int h : heights) {
-            low = Math.min(low, h);
-            high = Math.max(high, h);
+        int max=heights[0];
+        int min=heights[0];
+        for(int i=0;i<heights.length;i++)
+        {
+            max=Math.max(max,heights[i]);
+            min=Math.min(min,heights[i]);
         }
-        
-        int ans = Integer.MAX_VALUE;
-        
-        while (low <= high) {
-            int mid = (low + high) / 2;
-
-            int costMid = helper(heights, cost, mid);
-            int costLeft = helper(heights, cost, mid - 1);
-            int costRight = helper(heights, cost, mid + 1);
-
-            ans = Math.min(ans, costMid);
-
-            // Move in direction of lower cost
-            if (costLeft < costMid) {
-                high = mid - 1;
-            } else if (costRight < costMid) {
-                low = mid + 1;
-            } else {
-                break; // Found local minimum
+        int ans=Integer.MAX_VALUE;
+        while(min<=max)
+        {
+            int mid=min+(max-min)/2;
+            
+            int currCost1=totalCost(heights,cost,mid-1);
+            int currCost2=totalCost(heights,cost,mid);
+            int currCost3=totalCost(heights,cost,mid+1);
+            if(currCost2<=currCost1&&currCost2<=currCost3)
+            return currCost2;
+            
+            if(currCost1>=currCost2&&currCost2>=currCost3)
+            {
+                min=mid+1;
+            }
+            else{
+                max=mid-1;
             }
         }
-        
-        
         return ans;
     }
-    
-    
-    
-    private int helper(int[] heights, int[] cost, int targetHeight) {
-        int totalCost = 0;
-        for (int i = 0; i < heights.length; i++) {
-            totalCost += Math.abs(heights[i] - targetHeight) * cost[i];
+    public int totalCost(int []arr,int[]cost,int h)
+    {
+        int currCost=0;
+        for(int i=0;i<arr.length;i++)
+        {
+            int diff=Math.abs(arr[i]-h);
+            currCost+=diff*cost[i];
         }
-        return totalCost;
+        return currCost;
     }
 }
